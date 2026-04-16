3b1dd3fbc08a8c4524a113574ecd6cc553df379e
Bautista Puertas <puertasbauti7@gmail.com>
2026-04-16 20:25:57 +0000

    ejercicio1:variables y operaciones

---
+++ ejercicios/app.js
@@ -0,0 +1,10 @@
+const nombre='Bautista';
+const edad=21;
+const materia= 'desarrollo de software';
+
+console.log(`hola, me llamo ${nombre} y tengo ${edad} años, estoy cursando ${materia}`);
+let contador=0;
+contador++;
+contador++;
+contador++;
+console.log(`el valor final del contador es ${contador}`);
